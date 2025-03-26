e001da7183378f7fc992fddcdc4f0e22f652c376
Noe <noe.esp@hotmail.fr>
2025-03-27 00:04:00 +0100

    chore(TASK-01): configure ci lint and formatter

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  "**/*.+(ts|tsx|css|md|json|yml|yaml|js|html)": "prettier --write",
+  "**/*.+(ts|tsx)": [
+    "eslint --fix",
+    // cf: https://github.com/okonet/lint-staged/issues/825#issuecomment-620018284
+    // --skipLibCheck will skip type checking of declaration files and save time during pre-commit hook
+    () => "tsc --noEmit --skipLibCheck"
+  ]
+}
