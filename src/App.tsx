a3381fb0f2bec7221e2ce3c81f3a77f2dfbdbe80
Noé <noe.esp@hotmail.fr>
2025-07-07 11:09:42 +0200

    feat(3): create Grid system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+
 import type { FC } from "react";
+import Grid from "./components/grid/Grid.component";
+import Header from "./components/header/Header.component";
 
 const App: FC = () => {
-  return <main className="h-full w-full bg-gray-200" />;
+  return (
+    <main className="relative flex min-h-screen flex-col content-center bg-red-100 ">
+      <Header />
+      <Grid />
+    </main>
+  );
 };
 
 export default App;
---
+++ src/components/header/Header.component.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+export const Header: FC = () => {
+  return <div className="flex flex-row items-center justify-center bg-green-100">Header</div>;
+};
+
+export default Header;
