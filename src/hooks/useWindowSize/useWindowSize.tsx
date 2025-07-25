6e82d99efe246089e3f2a45f2d5d614d3ffe4146
Noé <noe.esp@hotmail.fr>
2025-07-26 00:44:12 +0200

    feat(10): create Linkedin Card

--- src/hooks/useWindowSize/useWindowSize.tsx
+++ src/hooks/useWindowSize/useWindowSize.tsx
@@ -1,20 +1,29 @@
 import { useEffect, useState } from "react";
 
 export type ScreenSize = "xs" | "sm" | "md" | "lg" | "";
+export type ScreenInformation = {
+  screenSize: ScreenSize;
+  isMobile: boolean;
+};
 
-export const useScreenSize: () => ScreenSize = () => {
+export const useScreen: () => ScreenInformation = () => {
   const [screenSize, setScreenSize] = useState<ScreenSize>("");
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 376) {
         setScreenSize("xs");
+        setIsMobile(true);
       } else if (window.innerWidth >= 376 && window.innerWidth < 768) {
         setScreenSize("sm");
+        setIsMobile(true);
       } else if (window.innerWidth >= 768 && window.innerWidth < 1280) {
         setScreenSize("md");
+        setIsMobile(false);
       } else if (window.innerWidth >= 1280) {
         setScreenSize("lg");
+        setIsMobile(false);
       }
     };
 
@@ -24,5 +33,5 @@ export const useScreenSize: () => ScreenSize = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  return screenSize;
+  return { screenSize, isMobile };
 };
