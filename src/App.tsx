84c187ee779652e8779f60d255bf9cc087974d59
Noé <noe.esp@hotmail.fr>
2025-09-30 10:04:47 +0200

    feat: restart project from blank sheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import "./App.css";
 
 import type { FC } from "react";
-import Grid from "./components/grid/Grid.component";
-import Header from "./components/header/Header.component";
 
 const App: FC = () => {
-  return (
-    <main className="relative flex min-h-screen flex-col content-center bg-red-100 ">
-      <Header />
-      <Grid />
-    </main>
-  );
+  return <main className=" bg-red-100 ">Portfolio</main>;
 };
 
 export default App;
--- src/components/header/Header.component.tsx
+++
@@ -1,7 +0,0 @@
-import type { FC } from "react";
-
-export const Header: FC = () => {
-  return <div className="flex flex-row items-center justify-center bg-green-100">Header</div>;
-};
-
-export default Header;
